11acc9aff47ef77472b0ef2bf080411cade3bc81
solak-gamestudios <42231290+solak-gamestudios@users.noreply.github.com>
2018-10-24 18:32:48 +0300

    Update Ads.cs

--- Ads.cs
+++ Ads.cs
@@ -9,29 +9,19 @@ public class Ads : MonoBehaviour {
 	private BannerView bannerView;
 	private InterstitialAd interstitial;
     GameObject Player;
-    //public Button AddScoreButton;
-    //public GameObject AskFor;
 
 
     // Use this for initialization
     void Start () {
         DontDestroyOnLoad(gameObject);
 		if (SceneManager.GetActiveScene ().name == "Main Menu") {
-			bannerView = new BannerView ("ca-app-pub-8696786032145652/4056105724", AdSize.SmartBanner, AdPosition.Bottom);
+			bannerView = new BannerView ("ca-app-pub-", AdSize.SmartBanner, AdPosition.Bottom);
 			bannerView.LoadAd (new AdRequest.Builder().Build ());
 		} 
 		if (SceneManager.GetActiveScene ().name == "Endless" && globalValues.minutes >= 2) {
-			interstitial = new InterstitialAd("ca-app-pub-8696786032145652/5151062528");
+			interstitial = new InterstitialAd("ca-app-pub-");
 			interstitial.LoadAd(new AdRequest.Builder().Build());
 		}
-		/*if (SceneManager.GetActiveScene ().name == "Main Menu") {
-
-			RewardBasedVideoAd rewardBasedVideo = RewardBasedVideoAd.Instance;
-			AdRequest request = new AdRequest.Builder ()
-			.Build ();
-			rewardBasedVideo.LoadAd (request, "ca-app-pub-8696786032145652/5552858525");
-
-		}*/
 	}
 	
 	// Update is called once per frame
@@ -55,29 +45,8 @@ public class Ads : MonoBehaviour {
                 globalValues.time = 0;
             }
         }
-		/*
-		if (rewardBasedVideo.IsLoaded ()) {
-			AddScoreButton.interactable = true;
-
-		} else {
-			AddScoreButton.interactable = false;
-		}
-		Debug.Log ("Request: " + rewardBasedVideo.IsLoaded ());
-		*/
 	}
 	void OnDestroy(){
 		bannerView.Hide ();
 	}
-	/*
-	public void ShowMoney(){
-		AskFor.SetActive (false);
-		rewardBasedVideo.Show();
-	}
-	public void ShowMoneyNo(){
-	AskFor.SetActive (false);
-	}
-	public void AskForMoney(){
-		AskFor.SetActive (true);
-	}
-	*/
 }
